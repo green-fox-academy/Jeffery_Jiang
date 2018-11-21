656821b2e2a6d4c28168a159a5577d68ea272028
Jeffery Jiang <Jeffery_Jiang@epam.com>
2018-11-21 11:18:18 +0800

    day2 wrokshop

---
+++ week3/day2/Filelogger/Filelogger/NetworkAndFileloger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.IO;
+
+namespace Fileloggertest
+{
+    class NetworkAndFileloger:Filelogger
+    {
+        private readonly HttpClient httpClient;
+
+        public NetworkAndFileloger(string filepath) : base(filepath)
+        {
+            httpClient=new HttpClient();
+        }
+
+        public override void Log( string massage)
+        {
+           httpClient.PostAsync("http://requestbin.fullcontact.com/1e517781", new StringContent(massage)).Wait();
+
+            
+        }
+    }
+}
